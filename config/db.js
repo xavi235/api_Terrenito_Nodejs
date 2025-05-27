572887fefcaf9260f180fdb183c533df63002d89
xavi235 <linkshaoran24@gmail.com>
2025-05-26 23:38:07 -0400

    s

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'Terrenos'
+  host: 'byn86ln264izy3ppnkdt-mysql.services.clever-cloud.com',
+  user: 'ukgvixfa1lq6nvxm',
+  password: '11HASD8AmwZjSw1yXJnO',
+  database: 'byn86ln264izy3ppnkdt'
 });
 
 db.connect((err) => {
